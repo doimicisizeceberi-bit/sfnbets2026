6721e9ff41ec7f3b38028056d9807bf21c92f539
Marius Cioiu <doimicisizeceberi@gmail.com>
2026-06-11 22:05:42 +0300

    v9

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,37 +1,48 @@
 import { SignJWT } from 'jose'
-import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
 
 const secret = new TextEncoder().encode(
   process.env.SESSION_SECRET
 )
 
 export async function POST(req: Request) {
+
   const body = await req.json()
 
-  if (body.password !== process.env.SITE_PASSWORD) {
-    return new Response('Unauthorized', {
-      status: 401,
-    })
+  if (
+    body.password !== process.env.SITE_PASSWORD
+  ) {
+    return new NextResponse(
+      'Unauthorized',
+      { status: 401 }
+    )
   }
 
   const token = await new SignJWT({
     authenticated: true,
   })
-    .setProtectedHeader({ alg: 'HS256' })
+    .setProtectedHeader({
+      alg: 'HS256',
+    })
     .setExpirationTime('7d')
     .sign(secret)
 
-  const cookieStore = await cookies()
+  const response =
+    NextResponse.json({
+      success: true,
+    })
 
-	cookieStore.set('session', token, {
-	  httpOnly: true,
-	  secure: true,
-	  sameSite: 'lax',
-	  path: '/',
-	  maxAge: 60 * 60 * 24 * 7,
-	})
+  response.cookies.set(
+    'session',
+    token,
+    {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    }
+  )
 
-  return Response.json({
-    success: true,
-  })
+  return response
 }
